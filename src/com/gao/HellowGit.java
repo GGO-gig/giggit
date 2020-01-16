23d6cd1bda535e20aae34dbc9c9e57c8861ad41e
ming <ming@DESKTOP-VTM3H0F>
2020-01-16 11:42:18 +0800

    test

--- src/com/gao/HellowGit.java
+++ src/com/gao/HellowGit.java
@@ -3,6 +3,7 @@ package com.gao;
 public class HellowGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello  Git");
+		System.out.println("Hello  Git  ggg");
+		System.out.println("随便改改");
 	}
 }
